b738d67f69cb1860b04ac19b42f0226b5a0072ec
Arthur <arthur_s_farias@estudante.sesisenai.org.br>
2025-03-20 10:08:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+/*
+let n1 = prompt("Digite um numero")
+
+function verificarPar(n1) {
+    if (n1 % 2 == 0) {
+        console.log("Par");
+    } else {
+        console.log("Impar");
+    }
+}
+
+verificarPar(n1);
+*/
+
+
+
+/*
+function calcular(n1,n2) {
+    const soma = n1 + n2;
+    console.log(soma)
+
+    const sub = n1 - n2;
+    console.log(sub)
+
+    const mult = n1 * n2;
+    console.log(mult)
+
+    const div = n1 / n2;
+    console.log(div)
+};
+
+
+let num1 = parseFloat(prompt("Digite um numero"))
+let num2 = parseFloat(prompt("Digite outro numero"))
+calcular(num1,num2);
+*/
+
+while (i = 10) {
+    
+}
+
