54775985739a30bb7b574ff938abc72a4f95f897
kageituchero <arthur_s_farias@estudante.sesisenai.org.br>
2025-03-21 09:59:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//1. Verificação de Par ou Ímpar:
 /*
 let n1 = prompt("Digite um numero")
 
@@ -14,6 +15,7 @@ verificarPar(n1);
 
 
 
+//2. Operações Matemáticas Simples:
 /*
 function calcular(n1,n2) {
     const soma = n1 + n2;
@@ -35,7 +37,89 @@ let num2 = parseFloat(prompt("Digite outro numero"))
 calcular(num1,num2);
 */
 
-while (i = 10) {
-    
+
+
+//3. Laço While para Contagem Regressiva:
+/*
+let n = 11;
+let x = 0
+
+while (n > 1) {
+    n--;
+    x -= n
+  console.log(n);
 }
+*/
+
+
+
+//4. Manipulação de Strings:
+/*
+let str = 'JavaScript'
+
+function inverterTexto(str) {
+    var splitString = str.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    console.log(joinArray)
+}
+ inverterTexto(str)
+*/
+
+
+
+//5. Contagem de Caracteres em uma String:
+/*
+let str = 'cachorro'
+
+function contarCaracters(){
+    let myString = str.length;
+    console.log(myString);
+}
+contarCaracters(str)
+*/
+
+
+
+//6. Acessando Propriedades de Objetos:
+/*
+var meuCarro = new Object();
+meuCarro.fabricacao = "Ford";
+meuCarro.modelo = "Pinto";
+meuCarro.ano = 1974;
+
+console.log(meuCarro);
+*/
+
+
+
+//7. Função com Parâmetros Opcionais:
+/*
+let nome = prompt('digite seu nome')
+function mensagemPersonalizada(nome, mensagem = 'Olá'){ 
+    console.log(mensagem + ' ' + nome)
+}
+mensagemPersonalizada(nome, mensagem = 'Olá')
+*/
+
+
+
+//8. Calculadora de Média:
+/*
+let n1 = parseInt(prompt('Digite o primeiro número'))
+let n2 = parseInt(prompt('Digite o segundo número'))
+let n3 = parseInt(prompt('Digite o terceiro número'))
+
+function media(n1,n2,n3){
+    let media
+    let soma
+    soma = n1+n2+n3
+    media = soma/3
+console.log(soma)
+console.log(media)
+
+}
+media(n1,n2,n3)
+*/
+
 
